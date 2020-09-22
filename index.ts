40e823c58c52f41e07f12312311c8845b08af3a9
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-22 16:45:45 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Observable, interval } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 interval(10000).subscribe( 
-     n => ajax("https://api.icndb.com/jokes/random").
-	        subscribe( data => console.log(data.response.value.joke) 
+     n => ajax("https://api.chucknorris.io/jokes/random").
+	        subscribe( data => console.log(data.response.value) 
 	 ) 
 );
