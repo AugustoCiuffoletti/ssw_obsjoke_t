3d9b9c3c5e378e4e8e630fc3fe49c17b6b93e702
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-23 09:52:17 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 // Produce una battuta ogni 10 secondi
-import { Observable, interval } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
+import { Observable, interval } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-interval(10000).subscribe( 
-     n => ajax("https://api.chucknorris.io/jokes/random").
-	        subscribe( data => console.log(data.response.value) 
-	 ) 
+interval(10000).subscribe(n =>
+  ajax("https://api.chucknorris.io/jokes/random").subscribe(data =>
+    console.log(data.response.value)
+  )
 );
